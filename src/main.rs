5ab478bff7b6585790c1c1314cad21b8f3e2f674
wjcrumbleholme <64094813+wjcrumbleholme@users.noreply.github.com>
2025-05-26 20:28:13 +0100

    Buttons and padding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use macroquad::prelude::*;
-use ui::{Alignment, Container, Position, Size, UIElement};
+use macroquad::{prelude::*};
+use ui::{Alignment, Button, Container, Label, Padding, Position, Size, UIElement};
 
 pub mod ui;
 
@@ -40,9 +40,32 @@ async fn main() {
         Size::Abs(50.0),
         RED,
     );
+    child2.add_padding(Padding::uniform(5.0));
 
+    let example_text = Label::new(
+        Position::Align(Alignment::LeTop),
+        Position::Align(Alignment::Centre), 
+        16, 
+        "Test".to_string(), 
+        WHITE,
+    );
+
+    let example_btn = Button::new(
+        Position::Align(Alignment::Centre), 
+        Position::Rel(0.7), 
+        Size::Abs(400.0), 
+        Size::Abs(100.0), 
+        LIME, 
+        BLACK, 
+        "Click Me!".to_string(), 
+        32, 
+        || {println!("Button has been clicked!")},
+    );
+
+    child2.add_child(Box::new(example_text));
     child.add_child(Box::new(child2));
     root.add_child(Box::new(child));
+    root.add_child(Box::new(example_btn));
 
 
     loop {
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,4 @@
-use macroquad::{color::{Color}, shapes::draw_rectangle};
+use macroquad::{color::Color, input::{is_mouse_button_pressed, mouse_position, MouseButton}, shapes::draw_rectangle, text::{draw_text, measure_text}};
 
 
 // Align along an axis
@@ -30,6 +30,38 @@ impl Position {
     }
 }
 
+pub struct Padding {
+    top: f32,
+    bottom: f32,
+    left: f32,
+    right: f32,
+}
+
+impl Padding {
+    pub fn uniform(value: f32) -> Self {
+        Self {
+            top: value,
+            bottom: value,
+            left: value,
+            right: value,
+        }
+    }
+
+    pub fn zero() -> Self {
+        Self::uniform(0.0)
+    }
+
+    pub fn new(top: f32, bottom: f32, left: f32, right: f32) -> Self {
+        Self {
+            top: top,
+            bottom: bottom,
+            left: left,
+            right: right,
+        }
+    }
+
+}
+
 // Size of the object
 pub enum Size {
     Abs(f32),
@@ -53,26 +85,93 @@ pub struct Container {
     w: Size,
     h: Size,
     color: Color,
+    padding: Padding
 
 }
 
 // Basic container object logic
 impl Container {
-    pub fn new(x: Position, y: Position, w: Size, h: Size, color: Color) -> Container{
-        Container { 
+    pub fn new(x: Position, y: Position, w: Size, h: Size, color: Color) -> Self{
+        Self { 
             children: vec![], 
             x: x,
             y: y,
             w: w,
             h: h,
-            color: color
+            color: color,
+            padding: Padding::zero()
         }
     }
 
+    /// When calling, wrap the object in a box Box::new()
     pub fn add_child(&mut self, child: Box<dyn UIElement>) {
         self.children.push(child);
     }
+
+    /// Adds padding to a container
+    pub fn add_padding(&mut self, padding:Padding) {
+        self.padding = padding
+    }
 }
+
+
+// Label
+pub struct Label {
+    text: String,
+    x: Position,
+    y: Position,
+    font_size: u16,
+    color: Color
+}
+
+// Label logic
+impl Label {
+    pub fn new(x: Position, y: Position, font_size: u16, text: String, color: Color) -> Self {
+        Self { 
+            text: text, 
+            x: x, 
+            y: y, 
+            font_size: font_size, 
+            color: color,
+        }
+    }
+}
+
+// Button
+pub struct Button {
+    label: Label,
+    x: Position,
+    y: Position,
+    w: Size,
+    h: Size, 
+    bg_color: Color,
+    on_click: Box<dyn Fn()>,
+}
+
+impl Button {
+    pub fn new<F: Fn() + 'static>(x: Position, y: Position, w: Size, h: Size, bg_color: Color, txt_color: Color, txt_content:String, txt_size: u16, on_click: F) -> Self {
+        let label = Label::new(
+            Position::Align(Alignment::Centre), 
+            Position::Align(Alignment::Centre), 
+            txt_size, 
+            txt_content, 
+            txt_color,
+        );
+        Self { 
+            label: label, 
+            x: x, 
+            y: y, 
+            w: w, 
+            h: h, 
+            bg_color: bg_color, 
+            on_click: Box::new(on_click), 
+        }
+    }
+}
+
+
+
+
 
 // Define what all UIElements need to have 
 pub trait UIElement {
@@ -91,10 +190,54 @@ impl UIElement for Container {
 
         draw_rectangle(x, y, w, h, self.color);
 
+        let content_x = x + self.padding.left;
+        let content_y = y + self.padding.top;
+        let content_w = w - self.padding.left - self.padding.right;
+        let content_h = h - self.padding.left - self.padding.right;
+
         // Recursively draw the child's objects
         for child in &self.children {
-            child.draw(x, y, w, h);
+            child.draw(content_x, content_y, content_w, content_h);
         }
+    }
+}
+
+//Implement the trait for the Label object
+impl UIElement for Label {
+    fn draw(&self, parent_x: f32, parent_y: f32, parent_w: f32, parent_h: f32) {
+        let text_dim = measure_text(&self.text, None, self.font_size, 1.0);
+        let w = text_dim.width;
+        let h = text_dim.height;
+
+        let x = self.x.coord(parent_x, parent_w, w);
+        let y = self.y.coord(parent_y, parent_h, h);
+
+        draw_text(&self.text, x, y + h, self.font_size as f32, self.color);
+    }
+}
+
+//Implement the trait for the Button object
+impl UIElement for Button {
+    fn draw(&self, parent_x: f32, parent_y: f32, parent_w: f32, parent_h: f32) {
+        let w = self.w.caclulate(parent_w);
+        let h = self.h.caclulate(parent_h);
+        let x = self.x.coord(parent_x, parent_w, w);
+        let y = self.y.coord(parent_y, parent_h, h);
+
+        // Check if clicked
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let(mouse_x, mouse_y) = mouse_position();
+            if mouse_x >= x && mouse_x <= (x + w) && mouse_y >= y && mouse_y <= (y+h) {
+                (self.on_click)();
+            }
+        }
+
+        // Draw background
+        draw_rectangle(x, y, w, h, self.bg_color);
+
+        // Draw label in button
+        self.label.draw(x, y, w, h);
+
     }
 }
 
